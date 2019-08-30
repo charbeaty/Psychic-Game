ae7d29b1a25465f058539773c9af944e4126fc21
Charissa Beaty <char.beaty11@gmail.com>
2019-08-29 23:07:59 -0600

    Added jumbotron and columns

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,7 +6,7 @@
 
         //sets up the answerArray to show how many letters there are;
         //using _'s;
-
+        var words = ["Friends", "The Fresh Prince of Belair", "Saved By the Bell", "Boy Meets World", "Buffy the Vampire Slayer", "Full House", "The Simpsons", "Seinfeld", "Home Improvement", "Frasier", "Sabrina the Teenage Witch", "Everybody Loves Raymond", "Roseanne", "Sex and the City", "The King of Queens", "Charmed", "Dawsons Creek", "The Nanny", "Dinosaurs", "Animaniacs", "Hey Arnold", "Pinky and the Brain", "Doug", "Recess", "Sister Sister", "Walker Texas Ranger", "Growing Pains", "The Cosby Show", "Magic School Bus", "Arthur", "Powerpuff Girls", "Spongebob Squarepants"];
         var answerArray ={};
         for(var i = 0; i < word.lenth; i++) {
          answerArray[i] = "_";
