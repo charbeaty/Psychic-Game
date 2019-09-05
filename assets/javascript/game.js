7b944d2d7bce054b8f2c28752fedcc8820a1fb0c
Charissa Beaty <char.beaty11@gmail.com>
2019-09-04 23:40:10 -0600

    added some JavaScript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,21 @@
-$(document).ready(function () {
-    
- 
+var start = document.getElementById("start");
+
+document.onkeyup = function(event) {
+
+if (event.keyCode >= 65 && event.keyCode <= 90){
+   
 }
-});
-    
+};    
       
     
 
         //var words = ["Friends", "The Fresh Prince of Belair", "Saved By the Bell", "Boy Meets World", "Buffy the Vampire Slayer", "Full House", "The Simpsons", "Seinfeld", "Home Improvement", "Frasier", "Sabrina the Teenage Witch", "Everybody Loves Raymond", "Roseanne", "Sex and the City", "The King of Queens", "Charmed", "Dawsons Creek", "The Nanny", "Dinosaurs", "Animaniacs", "Hey Arnold", "Pinky and the Brain", "Doug", "Recess", "Sister Sister", "Walker Texas Ranger", "Growing Pains", "The Cosby Show", "Magic School Bus", "Arthur", "Powerpuff Girls", "Spongebob Squarepants"];
         //var answerArray ={};
         //for(var i = 0; i < word.length; i++) {
-         //answerArray[i] = "_";}
+
+        // step one: declare all goal variables
+        //step two: write function to reset variables (guesses, start of game)
+        //step three: write function to compare your guess against computer
+        //make decisions based on that
+        //use key up function to make decisions on user's choice
+        //take var update DOM elements (wins, reamining, guessed)
