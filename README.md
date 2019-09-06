5cc01fbbca4c33dcffa5fb51c259129c9d632a41
Charissa Beaty <char.beaty11@gmail.com>
2019-09-05 22:50:27 -0600

    Added 'letters guessed' in game.js

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
-# Word-Guess-Game
+# Psychic-Game
 
-This is a Word Guess Game, similar to hangman. While there is no stick figure on the gallows, your wins and losses will be recorded.
-
-The theme of this word game is "90's television shows". If you have trouble guessing the names of the T.V. shows, feel free to use google. 
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,24 @@
-var start = document.getElementById("start");
 
+var guess = document.getElementById("guess");
+var win = document.getElementById("win");
+//# of times user guessed letter correctly
+var losses = document.getElementById("losses");
+//# of times user has failed to guess letter correctly
+var left = document.getElementById("left");
+//# of guesses left. This will update 
+var guessed = document.getElementById("guessed");
+//letter the user typed. Display until user wins or loses 
+//When player wins increase Wins counter, game starts over without restart
+//when player loses increase losses counter, start hame without page reset
+var userText = document.getElementById("user-guess");
 document.onkeyup = function(event) {
+        userText.textContent = event.key;
+};
 
-if (event.keyCode >= 65 && event.keyCode <= 90){
+
+
+
+ //(event.keyCode >= 65 && event.keyCode <= 90){
    
-}
-};    
-      
+  
     
-
-        //var words = ["Friends", "The Fresh Prince of Belair", "Saved By the Bell", "Boy Meets World", "Buffy the Vampire Slayer", "Full House", "The Simpsons", "Seinfeld", "Home Improvement", "Frasier", "Sabrina the Teenage Witch", "Everybody Loves Raymond", "Roseanne", "Sex and the City", "The King of Queens", "Charmed", "Dawsons Creek", "The Nanny", "Dinosaurs", "Animaniacs", "Hey Arnold", "Pinky and the Brain", "Doug", "Recess", "Sister Sister", "Walker Texas Ranger", "Growing Pains", "The Cosby Show", "Magic School Bus", "Arthur", "Powerpuff Girls", "Spongebob Squarepants"];
-        //var answerArray ={};
-        //for(var i = 0; i < word.length; i++) {
-
-        // step one: declare all goal variables
-        //step two: write function to reset variables (guesses, start of game)
-        //step three: write function to compare your guess against computer
-        //make decisions based on that
-        //use key up function to make decisions on user's choice
-        //take var update DOM elements (wins, reamining, guessed)
