f9968103c23288da30b23f8650cbd53e0c322e8c
Charissa Beaty <char.beaty11@gmail.com>
2019-09-07 11:43:38 -0600

    Created function to reset game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,51 +1,56 @@
 
 //Array that lists all the options
-var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 
-'h', 'i', 'j', 'k','l', 'm', 'n', 'o', 'p', 'q', 
-'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g',
+        'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
+        'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 //Variables for tracking wins, losses, and guesses left
 var wins = 0;
 var losses = 0;
 var guess = 12;
 
-
-
-//# of guesses left. This will update 
-
-//letter the user typed. Display until user wins or loses 
-//When player wins increase Wins counter, game starts over without restart
-//when player loses increase losses counter, start hame without page reset
-
 //Variables that hold references to places in HTML where we want to display.
 var userWins = document.getElementById("user-wins");
+
 var userLosses = document.getElementById("user-losses");
+
 var guessLeft = document.getElementById("guess-left");
+
 var userChoice = document.getElementById("user-choice");
+
 console.log(userChoice);
-document.onkeyup = function(event) {
-       var userGuess = event.key;
-
-var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
- 
-console.log(computerChoice);  
-
-if(userGuess === computerChoice){
-        wins++;
-}else{
-        losses++;
+function startGame(){
+guess = 12  
+userWins.textContent = "Wins: " + wins;
+userLosses.textContent = "Losses: " + losses;
+guessLeft.textContent = "Guesses Left: " + guess;
+userChoice.textContent = "";
 }
-if(userGuess === computerChoice || userGuess !== computerChoice){
-        guess--;
-}
-if(guess === 0 {
+document.onkeyup = function (event) {
+        var userGuess = event.key;
+
+        var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+
+        console.log(computerChoice);
+
+
+        if (userGuess === computerChoice) {
+                wins++;
+                startGame();
+        }
+        if (userGuess === computerChoice || userGuess !== computerChoice) {
+                guess--;
+        }
+        if (guess <= 0){
+               losses++; 
+               startGame();
+        }
         
-        //code to restart game
-})
 
+                //make a variable for startGame
 
-        userWins.textContent = "Wins: " + wins;
-        userLosses.textContent = "Losses: " + losses;
-        guessLeft.textContent = "Guesses Left: " + guess;
-        userChoice.textContent = "You Guessed: " + userGuess;
-};
+                userWins.textContent = "Wins: " + wins;
+                userLosses.textContent = "Losses: " + losses;
+                guessLeft.textContent = "Guesses Left: " + guess;
+                userChoice.textContent = "You Guessed: " + userGuess;
+}
